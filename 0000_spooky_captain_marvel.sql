234d58ffbc9d1d0ffbb567feef0ef5793aa9e07f
bayeziddev <sayadmdbayezidhosan@gmail.com>
2026-07-05 15:54:01 +0600

    Add files via upload

---
+++ 0000_spooky_captain_marvel.sql
@@ -0,0 +1,20 @@
+{
+  "version": "7",
+  "dialect": "mysql",
+  "entries": [
+    {
+      "idx": 0,
+      "version": "5",
+      "when": 1783240585291,
+      "tag": "0000_spooky_captain_marvel",
+      "breakpoints": true
+    },
+    {
+      "idx": 1,
+      "version": "5",
+      "when": 1783240716970,
+      "tag": "0001_magical_marvel_zombies",
+      "breakpoints": true
+    }
+  ]
+}
